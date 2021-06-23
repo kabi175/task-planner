ca3e345fe2ab4d903448483ab05b16a117e3d236
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-06-23 15:54:42 +0530

    updated Activity and card

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import Card from './Card';
+import { CardColors } from './colors';
+
+const cards = [
+  {
+    task: 'Activity Tracker',
+    progress: 70,
+  },
+  {
+    task: 'b',
+    progress: 20,
+  },
+  {
+    task: 'c',
+    progress: 50,
+  },
+  {
+    task: 'd',
+    progress: 90,
+  },
+];
+
 const Activity: React.FC = () => {
   return (
     <View style={styles.contanier}>
-      <Text> Active Projects </Text>
-      <Card name={'Activity Tracker'} />
+      <Text style={styles.title}> Active Projects </Text>
+      <ScrollView horizontal={true}>
+        {cards.map((card, id) => {
+          return (
+            <TouchableOpacity
+              key={id}
+              onPress={() => console.log('Activity card clicked')}
+            >
+              <Card {...card} {...CardColors[id % CardColors.length]} />
+            </TouchableOpacity>
+          );
+        })}
+        <View style={styles.space} />
+      </ScrollView>
     </View>
   );
 };
@@ -13,9 +52,19 @@ const Activity: React.FC = () => {
 const styles = StyleSheet.create({
   contanier: {
     flex: 1,
-    backgroundColor: 'wheat',
+    backgroundColor: 'white',
     width: '100%',
-    padding: 25,
+  },
+  title: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingBottom: 20,
+    padding: 20,
+  },
+  space: {
+    height: 10,
+    width: 20,
   },
 });
 
---
+++ components/colors.ts
@@ -0,0 +1,23 @@
+export interface ICardColor {
+  bg: string;
+  bar: string;
+}
+
+export const CardColors: ICardColor[] = [
+  {
+    bg: '#66cdaa',
+    bar: '#98e3ca',
+  },
+  {
+    bg: '#eb6e6e',
+    bar: '#f59090',
+  },
+  {
+    bg: '#ffc908',
+    bar: '#ffde69',
+  },
+  {
+    bg: '#7da0fa',
+    bar: '#acc2fc',
+  },
+];
